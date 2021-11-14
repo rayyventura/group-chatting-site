32468acdf467094aa07623061ecfeb2e9f146f6c
Rayane Ventura <rayyventura@gmail.com>
2021-11-14 13:08:12 -0300

    Projeto Finalizado sem Bônus

--- Javascript/script.js
+++ Javascript/script.js
@@ -24,14 +24,15 @@ logarUsuario();
 let containerMensagem = document.querySelector('.conteudo');
 function tratarSucessoMensagem(resposta){
     containerMensagem.innerHTML='';
-for(let i=0;resposta.data.length; i++){
-    const tipoMensagem=resposta.data[i].type;
+    const tamanhoArray = resposta.data.length;
+    for(let i=0;i<tamanhoArray; i++){
+     const tipoMensagem=resposta.data[i].type;
     if(tipoMensagem==='message'){
-        containerMensagem.innerHTML +=` <div class="${resposta.data[i].type}"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  <span class='para'>para</span> <span class='destine'>${resposta.data[i].to}: </span> <span class="texto">${resposta.data[i].text}</span></div>`
+        containerMensagem.innerHTML +=` <div class="${resposta.data[i].type}" data-identifier="message"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  <span class='para'>para</span> <span class='destine'>${resposta.data[i].to}: </span> <span class="texto">${resposta.data[i].text}</span></div>`
     }else if(tipoMensagem==='status'){
         containerMensagem.innerHTML +=` <div class="${resposta.data[i].type}"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  ${resposta.data[i].text}</div>`
     }else if(tipoMensagem==='private_message'){
-        containerMensagem.innerHTML +=` <div class="${resposta.data[i].type}"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  <span class='para'>reservadamente para</span> <span class='destine'>${resposta.data[i].to}: </span> <span class="texto">${resposta.data[i].text}</div>`
+        containerMensagem.innerHTML +=` <div class="${resposta.data[i].type} display-none" data-identifier="message"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  <span class='para'>reservadamente para</span> <span class='destine'>${resposta.data[i].to}: </span> <span class="texto">${resposta.data[i].text}</div>`
     }
     const lastMessage=containerMensagem.lastElementChild;
     lastMessage.scrollIntoView({behavior:"smooth"});
@@ -41,4 +42,27 @@ for(let i=0;resposta.data.length; i++){
 function tratarErroMensagem(resposta){
 console.log(resposta.data);
 }
+
+function atualizarStatus(){
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',userName);
+}
+
+
+function enviarMensagem(){
+    let input = document.querySelector('input');
+    const message= {
+	from: nameUser,
+	to: "Todos",
+	text: input.value,
+	type: "message" 
+}
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', message);
+    promessa.then(tratarSucessoLogin);
+    promessa.catch(erroEnvioMensagem);
+    input.value='';
+}
+function erroEnvioMensagem(){
+    window.location.reload();
+}
 setInterval(tratarSucessoLogin,3000);
+setInterval(atualizarStatus,5000);
