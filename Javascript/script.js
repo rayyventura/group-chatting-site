e166cffe69ed71363bad9f48fc82736c3946407b
Rayane Ventura <rayyventura@gmail.com>
2021-11-17 01:46:39 -0300

    Bonus concluidos, faltando página inicial

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,14 @@
+const containerMensagem = document.querySelector('.conteudo');
 let nameUser = prompt("Qual o seu nome?")
-let enviarPara='';
+let enviarPara='Todos';
 let visibilidade='message';
-let containerMensagem = document.querySelector('.conteudo');
+
+document.querySelector('footer input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      enviarMensagem();
+    }
+});
+
 setInterval(tratarSucessoLogin,3000);
 setInterval(atualizarStatus,5000);
 setInterval(verificarParticipantes,10000);
@@ -37,11 +44,11 @@ function tratarSucessoMensagem(resposta){
         }else if(tipoMensagem==='status'){
             containerMensagem.innerHTML +=` <div class="${resposta.data[i].type}"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  ${resposta.data[i].text}</div>`
         }else if(resposta.data[i].from===nameUser || resposta.data[i].to == enviarPara || resposta.data[i].to ==='Todos'){
-           console.log(enviarPara);
             containerMensagem.innerHTML +=` <div class="${resposta.data[i].type}" data-identifier="message"><span class="time">(${resposta.data[i].time})</span>  <span class="origin">${resposta.data[i].from}</span>  <span class='para'>reservadamente para</span> <span class='destine'>${resposta.data[i].to}: </span> <span class="texto">${resposta.data[i].text}</div>`
     }
-    const lastMessage=containerMensagem.lastElementChild;
-    lastMessage.scrollIntoView({behavior:"smooth"});
+     const lastMessage=containerMensagem.lastChild;
+     lastMessage.scrollIntoView({behavior:"smooth"});
+    
 }
 }
 function tratarErroMensagem(resposta){
@@ -90,7 +97,6 @@ for(let i=0; i<resposta.data.length; i++){
     <img src="assets/person-circle 1.svg" alt="">
     <div class="text-envio">${resposta.data[i].name}</div>
     <img src="assets/Vector.svg" class="display-none check" alt="">
-
 </div>`
 }
 }
@@ -110,25 +116,23 @@ function sairOpcoes(){
     verOpcoes.classList.add('display-none')
 }
 function opcaoTodos(Todos){
-const check = Todos.querySelector('.check');
-check.classList.toggle('display-none');
-enviarPara='Todos'
+    const check = Todos.querySelector('.check');
+    check.classList.toggle('display-none');
+    enviarPara='Todos'
 }
 function opcaoPessoa(Pessoa){
     const check = Pessoa.querySelector('.check');
     check.classList.toggle('display-none');
     enviarPara=Pessoa.innerText;
-    console.log(enviarPara);
 }   
 function visivelPublico(publico){
     const check = publico.querySelector('.check');
     check.classList.toggle('display-none');
     visibilidade='message';
-    console.log(visibilidade);
 }
 function visivelReservado(privado){
     const check = privado.querySelector('.check');
     check.classList.toggle('display-none');
     visibilidade='private_message';
-    console.log(visibilidade);
     }
+    
